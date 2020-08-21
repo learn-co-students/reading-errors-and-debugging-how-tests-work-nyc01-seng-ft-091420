7232572cb0399120d93e0893535765275c7bad6b
Mike Smith <gooseisdead@mac.com>
2020-08-21 20:09:31 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,14 @@
-# Add your variables here
+first_number = 170
+second_number = 566
+
+sum = first_number + second_number
+puts "#{sum}"
+
+difference = first_number - second_number
+puts "#{difference}"
+
+product = first_number * second_number
+puts "#{product}"
+
+quotient = first_number / second_number
+puts "#{quotient}"
